f65d0a96f662995bbdfc85738d510ce2e94bf5d0
Ibrahim <diopib@gmail.com>
2019-12-12 09:22:17 -0500

    updated setup  project url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='pipeline-exec',
     version='0.1',
     packages=['pipeline_exec'],
-    url='pipeline-exec.readthedocs.io',
+    url='https://pipeline-exec.readthedocs.io/en/latest/',
     license='MIT',
     author='Sikilabs Technologies Inc',
     author_email='info@sikilabs.com',
